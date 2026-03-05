b2b5d9d88ed1ac05eea2692d71b266c93c32cd90
Andrei Senaev <andrei.senaev@gmail.com>
2026-03-05 14:09:23 +0100

    default message

--- scripts/make.sh
+++ scripts/make.sh
@@ -4,14 +4,14 @@ set -euo pipefail
 ROOT_DIR="$(cd "$(dirname "$0")/../" && pwd)"
 set -a; source "$ROOT_DIR/.env"; set +a
 
-echo "👉 [make] Starting deployment to production server"
+CONTROL_PLANE_SERVER_ADDRESS="$CONTROL_PLANE_SERVER_USERNAME@$CONTROL_PLANE_SERVER_IP"
+echo "👉 [make] Starting deployment to server=[$CONTROL_PLANE_SERVER_ADDRESS]"
 
 echo "👉 [make] Rsyncing provisioning files to server"
 $ROOT_DIR/scripts/rsync-provisioning.sh "$CONTROL_PLANE_SERVER_ADDRESS"
 echo "✅ [make] Provisioning files synced"
 
-CONTROL_PLANE_SERVER_ADDRESS="$CONTROL_PLANE_SERVER_USERNAME@$CONTROL_PLANE_SERVER_IP"
-echo "👉 [make] Deploying k8s cluster to server=[$CONTROL_PLANE_SERVER_ADDRESS]"
+echo "👉 [make] Deploying k8s cluster to server"
 ssh "$CONTROL_PLANE_SERVER_ADDRESS" "$PROVISIONING_PATH_LOCAL_TO_REMOTE/control-plane/scripts/bootstrap-control-plane.sh"
 echo "✅ [make] k8s cluster deployed to server"
 
