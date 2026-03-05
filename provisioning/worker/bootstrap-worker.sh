0de7340ae2ca4da9fb1551b662053bea75573ced
Andrei Senaev <andrei.senaev@gmail.com>
2026-03-05 11:44:40 +0100

    default message

--- provisioning/worker/bootstrap-worker.sh
+++ provisioning/worker/bootstrap-worker.sh
@@ -15,14 +15,14 @@ LABELS="$3"
 echo "👉 [bootstrap-worker] Bootstraping worker with labels=[${LABELS}]"
 
 mapfile -t _k3s_env < <(sudo bash -c 'source /etc/systemd/system/k3s-agent.service.env && echo "$K3S_TOKEN" && echo "$K3S_URL"')
-EXISTING_TOKEN="${_k3s_env[0]}"
-EXISTING_SERVER="${_k3s_env[1]}"
+EXISTING_NODE_TOKEN="${_k3s_env[0]}"
+EXISTING_SERVER_URL="${_k3s_env[1]}"
 
 # sudo curl -sfL https://get.k3s.io | K3S_URL="https://46.225.174.102:6443" K3S_TOKEN="K106ee9db4e0273d8811e91711470a74f74acddc3c49c7b74aa4600e95f9fd83a63::server:667cdaa00b52462d15e276f455aac14b"  sh -
 
 echo "👉 [bootstrap-worker] CONTROL_PLANE_SERVER_URL=[${CONTROL_PLANE_SERVER_URL}]"
 echo "👉 [bootstrap-worker] NODE_TOKEN=[${NODE_TOKEN}]"
-echo "👉 [bootstrap-worker] EXISTING_TOKEN=[${EXISTING_TOKEN}]"
-echo "👉 [bootstrap-worker] EXISTING_SERVER=[${EXISTING_SERVER}]"
+echo "👉 [bootstrap-worker] EXISTING_SERVER_URL=[${EXISTING_SERVER_URL}]"
+echo "👉 [bootstrap-worker] EXISTING_NODE_TOKEN=[${EXISTING_NODE_TOKEN}]"
 
 echo "✅ [bootstrap-worker] Bootstrapped worker with labels=[${LABELS}]"
