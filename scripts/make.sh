83a5755d7d70f51ecff6d4d3ae95bb73da8c2cb7
Andrei Senaev <andrei.senaev@gmail.com>
2026-03-06 14:33:26 +0100

    default message

--- scripts/make.sh
+++ scripts/make.sh
@@ -2,7 +2,6 @@
 set -euo pipefail
 
 ROOT_DIR="$(cd "$(dirname "$0")/../" && pwd)"
-set -a; source "$ROOT_DIR/.env"; set +a
 
 CONTROL_PLANE_SERVER_ADDRESS="${1}"
 echo "👉 [make] Starting deployment to server=[$CONTROL_PLANE_SERVER_ADDRESS]"
@@ -12,15 +11,15 @@ $ROOT_DIR/scripts/rsync-provisioning.sh "$CONTROL_PLANE_SERVER_ADDRESS"
 echo "✅ [make] Provisioning files synced"
 
 echo "👉 [make] Deploying k8s cluster to server"
-ssh "$CONTROL_PLANE_SERVER_ADDRESS" "$PROVISIONING_PATH_LOCAL_TO_REMOTE/control-plane/scripts/bootstrap-control-plane.sh"
+ssh "$CONTROL_PLANE_SERVER_ADDRESS" "~/k3s-cluster/provisioning/control-plane/scripts/bootstrap-control-plane.sh"
 echo "✅ [make] k8s cluster deployed"
 
 echo "👉 [make] Connecting to worker nodes"
 $ROOT_DIR/scripts/connect-all-workers.sh
 echo "✅ [make] Worker nodes connected"
 
-echo "👉 [make] Deploying k8s services"
-ssh "$CONTROL_PLANE_SERVER_ADDRESS" "$PROVISIONING_PATH_LOCAL_TO_REMOTE/control-plane/scripts/bootstrap-services.sh"
+echo "👉 [make] Deploying k8s services on control-plane=[$CONTROL_PLANE_SERVER_ADDRESS]"
+ssh "$CONTROL_PLANE_SERVER_ADDRESS" "~/k3s-cluster/provisioning/control-plane/scripts/bootstrap-services.sh"
 echo "✅ [make] k8s services deployed"
 
 echo "🏁 [make] Deployment completed successfully!"
