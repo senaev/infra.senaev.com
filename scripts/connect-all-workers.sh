725c4c4949938eece5cc204b8828e260c6c71608
Andrei Senaev <andrei.senaev@gmail.com>
2026-03-06 14:05:39 +0100

    default message

--- scripts/connect-all-workers.sh
+++ scripts/connect-all-workers.sh
@@ -11,7 +11,14 @@ NODE_TOKEN=$(ssh "$CONTROL_PLANE_SERVER_ADDRESS" "cat /var/lib/rancher/k3s/serve
 echo "✅ [connect-all-workers] NODE_TOKEN.length=[${#NODE_TOKEN}]"
 
 echo "👉 [connect-all-workers] Connecting to worker nodes"
-echo "$WORKERS" | jq -c '.[]' | while read -r obj; do
+
+worker_objs=()
+while IFS= read -r line; do
+  worker_objs+=( "$line" )
+done < <(echo "$WORKERS" | jq -c '.[]')
+echo "✅ [connect-all-workers] worker_objs.length=[${#worker_objs[@]}]"
+
+for obj in "${worker_objs[@]}"; do
   addr="$(echo "$obj" | jq -r '"\(.username)@\(.host)"')"
   labels="$(echo "$obj" | jq -r '.labels')"
   [[ -z "$addr" || "$addr" == "null" ]] && continue
