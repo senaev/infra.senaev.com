7a8c3e194119281d8d6d888a0cbec1fad9a9128a
Andrei Senaev <andrei.senaev@gmail.com>
2026-03-06 13:48:32 +0100

    default message

--- scripts/make.sh
+++ scripts/make.sh
@@ -15,6 +15,10 @@ echo "👉 [make] Deploying k8s cluster to server"
 ssh "$CONTROL_PLANE_SERVER_ADDRESS" "$PROVISIONING_PATH_LOCAL_TO_REMOTE/control-plane/scripts/bootstrap-control-plane.sh"
 echo "✅ [make] k8s cluster deployed"
 
+echo "👉 [make] Deploying k8s services"
+ssh "$CONTROL_PLANE_SERVER_ADDRESS" "$PROVISIONING_PATH_LOCAL_TO_REMOTE/control-plane/scripts/bootstrap-services.sh"
+echo "✅ [make] k8s services deployed"
+
 echo "👉 [make] Connecting to worker nodes"
 $ROOT_DIR/scripts/connect-all-workers.sh
 echo "✅ [make] Worker nodes connected"
