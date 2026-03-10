97510b729b631df5eb34162e8531b79975418636
Andrei Senaev <andrei.senaev@gmail.com>
2026-03-10 18:50:08 +0100

    add filebrowser

---
+++ provisioning/control-plane/scripts/upgrade-namespace.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+# Bootstrap k3s worker. If all conditions are satisfied, do nothing; otherwise uninstall and reinstall.
+if [[ $# -lt 1 ]]; then
+  echo "Usage: $0 <namespace>" >&2
+  echo "Example: $0 senaev-com" >&2
+  exit 1
+fi
+
+
+SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
+set -a; source "$SCRIPT_DIR/../../.env"; set +a
+
+NS="$1"
+CHART_PATH="$PROVISIONING_PATH_REMOTE/control-plane/helm/$NS"
+
+echo "👉 [upgrade-namespace] Checking namespace=[$NS]"
+kubectl create namespace "$NS" --dry-run=client -o yaml | kubectl apply -f -
+echo "✅ [upgrade-namespace] Namespace=[$NS] exists"
+
+echo "👉 [upgrade-namespace] Helm upgrade namespace=[$NS]"
+helm upgrade --install "$NS" $CHART_PATH \
+-n "$NS" \
+-f $CHART_PATH/values.yaml \
+--take-ownership
+echo "✅ [upgrade-namespace] Helm upgrade namespace=[$NS]"
