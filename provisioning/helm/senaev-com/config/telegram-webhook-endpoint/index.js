6395369210876782b45aaed4f5563aa86ed266de
senaev <senaev@yandex-team.ru>
2026-03-16 16:25:50 +0100

    add webhook receiver service

---
+++ provisioning/helm/senaev-com/config/telegram-webhook-endpoint/index.js
@@ -0,0 +1,24 @@
+const { Kafka } = require('kafkajs')
+
+const kafka = new Kafka({
+  brokers: [process.env.KAFKA_BROKERS]
+})
+
+const producer = kafka.producer()
+
+async function run() {
+  await producer.connect()
+  console.log('Connected to Kafka')
+  let i = 1
+  while (true) {
+    await producer.send({
+      topic: 'telegram-webhook-data',
+      messages: [{ value: `hello, world ${i}` }]
+    })
+    console.log(`Sent: hello, world ${i}`)
+    i++
+    await new Promise(resolve => setTimeout(resolve, 1000))
+  }
+}
+
+run().catch(console.error)
