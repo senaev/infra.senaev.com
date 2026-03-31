f99212a3a1418f751fac4d1bb9b64e0c393d3e9f
senaev <senaev@yandex-team.ru>
2026-03-31 17:07:15 +0200

    default message

--- supabase-list-notes/vite.config.ts
+++ supabase-list-notes/vite.config.ts
@@ -8,7 +8,7 @@ export default defineConfig({
         VitePWA({
             registerType: "autoUpdate",
             injectRegister: "auto",
-            includeAssets: ["favicon.ico", "logo.svg", "logo196.png", "robots.txt"],
+            includeAssets: ["favicon.ico", "logo.svg", "apple-touch-icon.png", "robots.txt"],
             manifest: {
                 name: "Supabase Notes",
                 short_name: "Notes",
@@ -19,9 +19,50 @@ export default defineConfig({
                 start_url: "/",
                 icons: [
                     {
-                        src: "/logo196.png",
-                        sizes: "196x196",
+                        src: "/icon-72x72.png",
+                        sizes: "72x72",
                         type: "image/png",
+                    },
+                    {
+                        src: "/icon-96x96.png",
+                        sizes: "96x96",
+                        type: "image/png",
+                    },
+                    {
+                        src: "/icon-128x128.png",
+                        sizes: "128x128",
+                        type: "image/png",
+                    },
+                    {
+                        src: "/icon-144x144.png",
+                        sizes: "144x144",
+                        type: "image/png",
+                    },
+                    {
+                        src: "/icon-152x152.png",
+                        sizes: "152x152",
+                        type: "image/png",
+                    },
+                    {
+                        src: "/icon-192x192.png",
+                        sizes: "192x192",
+                        type: "image/png",
+                    },
+                    {
+                        src: "/icon-384x384.png",
+                        sizes: "384x384",
+                        type: "image/png",
+                    },
+                    {
+                        src: "/icon-512x512.png",
+                        sizes: "512x512",
+                        type: "image/png",
+                    },
+                    {
+                        src: "/maskable-icon-512x512.png",
+                        sizes: "512x512",
+                        type: "image/png",
+                        purpose: "maskable",
                     },
                 ],
             },
