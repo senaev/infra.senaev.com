4a4fefec62816b8e0a4f61d7f8c8857d74a0e91e
Andrei Senaev <andrei.senaev@datadoghq.com>
2026-08-15 10:55:03 +0200

    @andrei.senaev default message

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -60,18 +60,3 @@ variable "VPS_FIRSTVDS_LABEL" {
   description = "FIRSTVDS worker VPS label"
   type        = string
 }
-
-variable "VPS_VULTR_HOST" {
-  description = "Vultr worker host"
-  type        = string
-}
-
-variable "VPS_VULTR_USERNAME" {
-  description = "Vultr worker SSH username"
-  type        = string
-}
-
-variable "VPS_VULTR_LABEL" {
-  description = "Vultr worker VPS label"
-  type        = string
-}
